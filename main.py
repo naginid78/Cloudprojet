680ec9c90be0bb1d4bd90ad62f41ffaf4eb33f39
naginid78 <58333761+naginid78@users.noreply.github.com>
2021-02-01 23:29:02 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from joblib import load
+from sklearn.ensemble import RandomForestRegressor
+
+import os
+import re
+
+app = FastAPI()
+
+
+def check_price(h:int=None, d:datetime=None):
+
+    reg = load('ml/clf.joblib')
+    data_input=[[hp,d]]
+    price=reg.predict(data_input)
+    return "The car you choose has an estimated price of ${}".format(price)
+    
+
+@app.get("/", response_class=HTMLResponse)
+def root(request:Request):
+    return templates.TemplateResponse("index.html",{"request": request})
+
+@app.post("/predict/", response_class=HTMLResponse)
+async def predict(request:Request, text: str = Form(...)):
+    result = check_msg(text)
+    return templates.TemplateResponse("predict.html",{"request": request, "text":text, "result":result})
