2997cc177103d40e0d110a66c9707a7de2f74447
naginid78 <58333761+naginid78@users.noreply.github.com>
2021-02-02 00:26:21 +0100

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -1,11 +1,11 @@
-from utils.utils import DataHandling, FeatureRecipe, FeatureExtractor, ModelBuilder
+from utils import DataHandling, FeatureRecipe, FeatureExtractor, ModelBuilder
 
 def DataManager(dh:DataHandling=None, fr:FeatureRecipe=None, fe:FeatureExtractor=None):
     """
         Function linking the 3 first classes of the pipeline
     """
     # Columns to keep
-    klist=['Manufacterer','Latest_Launch','Horsepower','Price_in_thousands']
+    klist=['Manufacturer','Latest_Launch','Horsepower','Price_in_thousands']
 
     dh = DataHandling()
     dh.get_data()
